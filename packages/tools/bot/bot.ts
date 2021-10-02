3ae102f6a551622824ef3d60b11e6d9a4576bd46
Waqas Kayani <mvickykayani@gmail.com>
2021-10-02 19:23:19 +0500

    update bot.ts and added api host

--- packages/tools/bot/bot.ts
+++ packages/tools/bot/bot.ts
@@ -43,7 +43,7 @@ export default class Bot {
     }
 
     private connect(): void {
-        let connection = io('ws://api-aws-app.emumbadevops.com', {
+        let connection = io('ws://127.0.0.1:9001', {
             forceNew: true,
             reconnection: false
         });
